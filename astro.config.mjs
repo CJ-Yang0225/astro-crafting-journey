29550ad32daabc57a47580f06a5d83600015a965
Jerry Yang <cj.yang0225@gmail.com>
2024-06-09 03:32:12 +0800

    chore: upgrade astro and add deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import vue from "@astrojs/vue";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind(), vue(), mdx(), sitemap()]
+});
